4e2d4396a4da39cdf32e0e6f019e41a451a7340e
BrunoDepetris <depetrisbruno@gmail.com>
2024-08-06 00:21:04 +0100

    EJERCICIO 3 TERMINADO

--- Ejercicio_Tres/Ejercicio_Tres/Form1.Designer.cs
+++ Ejercicio_Tres/Ejercicio_Tres/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             textBox_Salario.BackColor = SystemColors.InactiveBorder;
             textBox_Salario.BorderStyle = BorderStyle.FixedSingle;
-            textBox_Salario.Location = new Point(237, 173);
+            textBox_Salario.Location = new Point(222, 146);
             textBox_Salario.Name = "textBox_Salario";
             textBox_Salario.Size = new Size(313, 23);
             textBox_Salario.TabIndex = 0;
@@ -45,26 +45,28 @@
             // 
             // button_calcular
             // 
-            button_calcular.Location = new Point(117, 217);
+            button_calcular.Location = new Point(131, 198);
             button_calcular.Name = "button_calcular";
             button_calcular.Size = new Size(216, 23);
             button_calcular.TabIndex = 1;
             button_calcular.Text = "Calcular";
             button_calcular.UseVisualStyleBackColor = true;
+            button_calcular.Click += button_calcular_Click;
             // 
             // button_limpiar
             // 
-            button_limpiar.Location = new Point(429, 217);
+            button_limpiar.Location = new Point(421, 198);
             button_limpiar.Name = "button_limpiar";
             button_limpiar.Size = new Size(216, 23);
             button_limpiar.TabIndex = 2;
             button_limpiar.Text = "Limpiar";
             button_limpiar.UseVisualStyleBackColor = true;
+            button_limpiar.Click += button_limpiar_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(247, 298);
+            label1.Location = new Point(271, 274);
             label1.Name = "label1";
             label1.Size = new Size(51, 15);
             label1.TabIndex = 3;
@@ -74,7 +76,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(295, 298);
+            label2.Location = new Point(319, 274);
             label2.Name = "label2";
             label2.Size = new Size(13, 15);
             label2.TabIndex = 4;
@@ -84,7 +86,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(295, 298);
+            label3.Location = new Point(328, 274);
             label3.Name = "label3";
             label3.Size = new Size(13, 15);
             label3.TabIndex = 4;
@@ -94,7 +96,7 @@
             // label_mostrarSalario
             // 
             label_mostrarSalario.AutoSize = true;
-            label_mostrarSalario.Location = new Point(304, 298);
+            label_mostrarSalario.Location = new Point(328, 274);
             label_mostrarSalario.Name = "label_mostrarSalario";
             label_mostrarSalario.Size = new Size(13, 15);
             label_mostrarSalario.TabIndex = 5;
@@ -107,10 +109,10 @@
             ClientSize = new Size(800, 450);
             Controls.Add(label_mostrarSalario);
             Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(button_limpiar);
+            Controls.Add(label2);
             Controls.Add(button_calcular);
+            Controls.Add(label1);
             Controls.Add(textBox_Salario);
             Name = "Form1";
             Text = "Form1";
--- Ejercicio_Tres/Ejercicio_Tres/Form1.cs
+++ Ejercicio_Tres/Ejercicio_Tres/Form1.cs
@@ -1,9 +1,32 @@
 namespace Ejercicio_Tres {
     public partial class Form1 : Form {
+        private int salario = 0;
+        private int horas = 0;
+
+        private int resultado = 0;
+
         public Form1() {
             InitializeComponent();
         }
+        private void operaciones() {
+            switch (horas) {
+                case <= 40:
+                    salario = 150;
+                    resultado = horas * salario;
+                    break;
 
+                case >= 40:
+                    horas = horas - 40;
+                    salario = 250;
+                    resultado = horas * salario + 6000;
+                    break;
+
+            }
+
+
+
+            label_mostrarSalario.Text = resultado.ToString();
+        }
         private void label2_Click(object sender, EventArgs e) {
 
         }
@@ -14,6 +37,21 @@ namespace Ejercicio_Tres {
 
         private void textBox_Salario_TextChanged(object sender, EventArgs e) {
 
+
+        }
+
+        private void button_calcular_Click(object sender, EventArgs e) {
+            if (!int.TryParse(textBox_Salario.Text, out horas)) { //valido el legajo
+                MessageBox.Show("Ingrese el formato valido.");
+                return;
+            }
+            operaciones();
+
+        }
+
+        private void button_limpiar_Click(object sender, EventArgs e) {
+            textBox_Salario.Clear();
+            label_mostrarSalario.Text = "0";
         }
     }
 }
