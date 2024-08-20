4212acb5c4de9ee3d365d9586008b38c92769fb9
BrunoDepetris <depetrisbruno@gmail.com>
2024-08-20 12:59:27 +0100

    ejercicio cinco incompleto (no me dan mas el marote)

--- Ejercicio_Cinco/Form1.Designer.cs
+++ Ejercicio_Cinco/Form1.Designer.cs
@@ -28,58 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel_TextBoxCargar = new Panel();
             groupBox1 = new GroupBox();
+            button_Guardar = new Button();
+            textBox_Nota = new TextBox();
+            textBox_Nombre = new TextBox();
+            Nota = new Label();
             label1 = new Label();
-            Apellido = new Label();
+            panel_main = new Panel();
+            button_Calcular = new Button();
+            panel_TextBoxCargar.SuspendLayout();
             groupBox1.SuspendLayout();
+            panel_main.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel_TextBoxCargar
+            // 
+            panel_TextBoxCargar.Controls.Add(groupBox1);
+            panel_TextBoxCargar.Location = new Point(221, 90);
+            panel_TextBoxCargar.Name = "panel_TextBoxCargar";
+            panel_TextBoxCargar.Size = new Size(345, 215);
+            panel_TextBoxCargar.TabIndex = 6;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(Apellido);
+            groupBox1.Controls.Add(button_Guardar);
+            groupBox1.Controls.Add(textBox_Nota);
+            groupBox1.Controls.Add(textBox_Nombre);
+            groupBox1.Controls.Add(Nota);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(238, 95);
+            groupBox1.Location = new Point(11, 9);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(319, 186);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Carga Calificaciones";
             // 
+            // button_Guardar
+            // 
+            button_Guardar.Location = new Point(118, 131);
+            button_Guardar.Name = "button_Guardar";
+            button_Guardar.Size = new Size(75, 23);
+            button_Guardar.TabIndex = 4;
+            button_Guardar.Text = "Guardar";
+            button_Guardar.UseVisualStyleBackColor = true;
+            button_Guardar.Click += button_Guardar_Click;
+            // 
+            // textBox_Nota
+            // 
+            textBox_Nota.Location = new Point(84, 82);
+            textBox_Nota.Name = "textBox_Nota";
+            textBox_Nota.Size = new Size(211, 23);
+            textBox_Nota.TabIndex = 3;
+            // 
+            // textBox_Nombre
+            // 
+            textBox_Nombre.Location = new Point(84, 39);
+            textBox_Nombre.Name = "textBox_Nombre";
+            textBox_Nombre.Size = new Size(211, 23);
+            textBox_Nombre.TabIndex = 2;
+            // 
+            // Nota
+            // 
+            Nota.AutoSize = true;
+            Nota.Location = new Point(45, 85);
+            Nota.Name = "Nota";
+            Nota.Size = new Size(33, 15);
+            Nota.TabIndex = 1;
+            Nota.Text = "Nota";
+            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(27, 43);
+            label1.Location = new Point(27, 42);
             label1.Name = "label1";
             label1.Size = new Size(51, 15);
             label1.TabIndex = 0;
             label1.Text = "Nombre";
             // 
-            // Apellido
+            // panel_main
             // 
-            Apellido.AutoSize = true;
-            Apellido.Location = new Point(40, 85);
-            Apellido.Name = "Apellido";
-            Apellido.Size = new Size(51, 15);
-            Apellido.TabIndex = 1;
-            Apellido.Text = "Apellido";
+            panel_main.Controls.Add(button_Calcular);
+            panel_main.Controls.Add(panel_TextBoxCargar);
+            panel_main.Dock = DockStyle.Fill;
+            panel_main.Location = new Point(0, 0);
+            panel_main.Name = "panel_main";
+            panel_main.Size = new Size(800, 450);
+            panel_main.TabIndex = 6;
+            // 
+            // button_Calcular
+            // 
+            button_Calcular.Location = new Point(326, 342);
+            button_Calcular.Name = "button_Calcular";
+            button_Calcular.Size = new Size(139, 37);
+            button_Calcular.TabIndex = 7;
+            button_Calcular.Text = "Calcular";
+            button_Calcular.UseVisualStyleBackColor = true;
+            button_Calcular.Click += button_Calcular_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(groupBox1);
+            Controls.Add(panel_main);
             Name = "Form1";
             Text = "Form1";
+            panel_TextBoxCargar.ResumeLayout(false);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            panel_main.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-
+        private Panel panel_TextBoxCargar;
         private GroupBox groupBox1;
+        private Button button_Guardar;
+        private TextBox textBox_Nota;
+        private TextBox textBox_Nombre;
+        private Label Nota;
         private Label label1;
-        private Label Apellido;
+        private Panel panel_main;
+        private Button button_Calcular;
     }
 }
--- Ejercicio_Cinco/Form1.cs
+++ Ejercicio_Cinco/Form1.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 namespace Ejercicio_Cinco
 {
     public partial class Form1 : Form
@@ -5,6 +7,128 @@ namespace Ejercicio_Cinco
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < Alumno.Length; i++)
+            {
+                Alumno[i] = new GestionAlumnos();
+            }
+
+            button_Calcular.Enabled = false;
+
         }
+        GestionAlumnos[] Alumno = new GestionAlumnos[3];
+        int contador = 0;
+
+        private void CalcularValores()
+        {
+            int maxNota = int.MinValue;
+            int minNota = int.MaxValue;
+            string nombreMaxNota = string.Empty;
+            string nombreMinNota = string.Empty;
+
+            string resultadoCargados = "Los alumnos Cargados son: \n";
+            for (int i = 0; i < Alumno.Length; i++)
+
+            {
+
+                if (Alumno[i].GetNota() > maxNota)
+                {
+                    maxNota = Alumno[i].GetNota();
+                    nombreMaxNota = Alumno[i].GetNombre();\
+
+                }else if (Alumno[i].GetNota() < minNota)
+                {
+                    maxNota = Alumno[i].GetNota();
+                    nombreMinNota = Alumno[i].GetNombre();
+                }
+
+                
+            }
+            for (int i = 0; i < Alumno.Length; i++) {
+
+                resultadoCargados += $"Nombre {i + 1}: {Alumno[i].GetNombre()} , Nota {i + 1} : {Alumno[i].GetNota()} \n ";
+            }
+
+                MessageBox.Show(resultadoCargados + $" \n La nota mas alta es la de {nombreMaxNota} obteniendo una nota de {maxNota} \n \n La nota mas baja fue la de {nombreMinNota} Obteniendo una nota de {minNota}");
+
+
+        }
+
+
+        private void button_Guardar_Click(object sender, EventArgs e)
+        {
+
+            if (contador < Alumno.Length)
+            {
+
+                bool validacion = !string.IsNullOrWhiteSpace(textBox_Nombre.Text) &&
+                             !int.IsNegative(int.Parse(textBox_Nota.Text)) && int.Parse(textBox_Nota.Text) >= 0 &&
+                             System.Text.RegularExpressions.Regex.IsMatch(textBox_Nombre.Text, @"^[a-zA-Z\s]+$") &&
+                              System.Text.RegularExpressions.Regex.IsMatch(textBox_Nota.Text, @"^\d+$");
+
+
+                if (validacion)
+                {
+
+                    Alumno[contador].SetNombre(textBox_Nombre.Text);
+                    Alumno[contador].SetNota(int.Parse(textBox_Nota.Text));
+
+                    MessageBox.Show("Alumno Guardado Correctamente");
+
+                    textBox_Nombre.Text = string.Empty;
+                    textBox_Nota.Text = string.Empty;
+                    contador++;
+
+                }
+                else
+                {
+                    MessageBox.Show("Por favor ingrese valores validos");
+                }
+
+
+            }
+            if (contador > 2)
+            {
+
+                MessageBox.Show("Todos los alumnos fueron cargados");
+
+                button_Calcular.Enabled = true;
+                button_Guardar.Enabled = false;
+            }
+
+
+
+        }
+
+        private void button_Calcular_Click(object sender, EventArgs e)
+        {
+            CalcularValores();
+        }
+    }
+
+    public class GestionAlumnos
+    {
+        private string Nombre = string.Empty;   
+        private int Nota = 0;
+
+        public void SetNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void SetNota(int nota) {
+            Nota = nota;
+        }
+
+        public int GetNota()
+        {
+            return Nota;
+        }
+
+        public string GetNombre()
+        {
+            return Nombre;
+        }
+
     }
 }
